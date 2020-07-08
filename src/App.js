0713ae99c1a25f18024189e2f86ea68a6ef697b5
baymax44 <smartbay2021@outlook.com>
2020-07-08 01:29:07 -0400

    init setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Articles from './components/Articles'
+import User from './components/User'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>React Skeletons</h1>
       </header>
+      <div className="content">
+        <Articles />
+        <User />
+      </div>
     </div>
   );
 }
---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function User() {
+  return (
+    <div className="user">
+      <h2>User Details</h2>
+    </div>
+  )
+}
