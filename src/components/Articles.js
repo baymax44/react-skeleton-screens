140f4082f599b1cffb4fe04020a61682e5336547
baymax44 <smartbay2021@outlook.com>
2020-07-17 01:44:27 -0400

    skeleton element

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import SkeletonElement from '../skeletons/SkeletonElement';
 
 export default function Articles() {
   const [articles, setArticles] = useState(null)
